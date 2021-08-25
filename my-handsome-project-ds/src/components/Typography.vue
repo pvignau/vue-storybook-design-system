3752f4c20e353b69eaf891f7947886687b9a8b59
Pierre-Yves Vignau <pyvignau@infopro-digital.com>
2021-08-25 12:24:21 +0200

    update typography style location

--- my-handsome-project-ds/src/components/Typography.vue
+++ my-handsome-project-ds/src/components/Typography.vue
@@ -13,7 +13,7 @@
 </template>
 
 <script>
-import { typography } from 'tokens/dist/index'
+import { typography } from '@mhp/tokens'
 
 export default {
   name: 'Typography',
@@ -33,126 +33,5 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
-  @import "~tokens/dist/sass/typography.scss";
-
-  h1 {
-    font-size: $headline1FontSize;
-    text-decoration: $headline1TextDecoration;
-    font-family: $headline1FontFamily;
-    font-weight: $headline1FontWeight;
-    font-style: $headline1FontStyle;
-    font-stretch: $headline1FontStretch;
-    letter-spacing: $headline1LetterSpacing;
-    line-height: $headline1LineHeight;
-    text-indent: $headline1ParagraphIndent;
-    border-spacing: $headline1ParagraphSpacing;
-    // $headline1FontStyleOld;
-    // $headline1TextCase;
-  }
-  h2 {
-    font-size: $headline2FontSize;
-    text-decoration: $headline2TextDecoration;
-    font-family: $headline2FontFamily;
-    font-weight: $headline2FontWeight;
-    font-style: $headline2FontStyle;
-    font-stretch: $headline2FontStretch;
-    letter-spacing: $headline2LetterSpacing;
-    line-height: $headline2LineHeight;
-    text-indent: $headline2ParagraphIndent;
-    border-spacing: $headline2ParagraphSpacing;
-    // $headline2FontStyleOld;
-    // $headline2TextCase;
-  }
-  h3 {
-    font-size: $headline3FontSize;
-    text-decoration: $headline3TextDecoration;
-    font-family: $headline3FontFamily;
-    font-weight: $headline3FontWeight;
-    font-style: $headline3FontStyle;
-    font-stretch: $headline3FontStretch;
-    letter-spacing: $headline3LetterSpacing;
-    line-height: $headline3LineHeight;
-    text-indent: $headline3ParagraphIndent;
-    border-spacing: $headline3ParagraphSpacing;
-    // $headline3FontStyleOld;
-    // $headline3TextCase;
-  }
-  h4 {
-    font-size: $headline4FontSize;
-    text-decoration: $headline4TextDecoration;
-    font-family: $headline4FontFamily;
-    font-weight: $headline4FontWeight;
-    font-style: $headline4FontStyle;
-    font-stretch: $headline4FontStretch;
-    letter-spacing: $headline4LetterSpacing;
-    line-height: $headline4LineHeight;
-    text-indent: $headline4ParagraphIndent;
-    border-spacing: $headline4ParagraphSpacing;
-    // $headline4FontStyleOld;
-    // $headline4TextCase;
-  }
-  h5 {
-    font-size: $headline5FontSize;
-    text-decoration: $headline5TextDecoration;
-    font-family: $headline5FontFamily;
-    font-weight: $headline5FontWeight;
-    font-style: $headline5FontStyle;
-    font-stretch: $headline5FontStretch;
-    letter-spacing: $headline5LetterSpacing;
-    line-height: $headline5LineHeight;
-    text-indent: $headline5ParagraphIndent;
-    border-spacing: $headline5ParagraphSpacing;
-    // $headline5FontStyleOld;
-    // $headline5TextCase;
-  }
-  h6 {
-    font-size: $headline6FontSize;
-    text-decoration: $headline6TextDecoration;
-    font-family: $headline6FontFamily;
-    font-weight: $headline6FontWeight;
-    font-style: $headline6FontStyle;
-    font-stretch: $headline6FontStretch;
-    letter-spacing: $headline6LetterSpacing;
-    line-height: $headline6LineHeight;
-    text-indent: $headline6ParagraphIndent;
-    border-spacing: $headline6ParagraphSpacing;
-    // $headline6FontStyleOld;
-    // $headline6TextCase;
-  }
-  p {
-    font-size: $paragraph1FontSize;
-    text-decoration: $paragraph1TextDecoration;
-    font-family: $paragraph1FontFamily;
-    font-weight: $paragraph1FontWeight;
-    font-style: $paragraph1FontStyle;
-    font-stretch: $paragraph1FontStretch;
-    letter-spacing: $paragraph1LetterSpacing;
-    line-height: $paragraph1LineHeight;
-    text-indent: $paragraph1ParagraphIndent;
-    border-spacing: $paragraph1ParagraphSpacing;
-  }
-  p.small{
-    font-size: $paragraph2FontSize;
-    text-decoration: $paragraph2TextDecoration;
-    font-family: $paragraph2FontFamily;
-    font-weight: $paragraph2FontWeight;
-    font-style: $paragraph2FontStyle;
-    font-stretch: $paragraph2FontStretch;
-    letter-spacing: $paragraph2LetterSpacing;
-    line-height: $paragraph2LineHeight;
-    text-indent: $paragraph2ParagraphIndent;
-    border-spacing: $paragraph2ParagraphSpacing;
-  }
-  p.smaller {
-    font-size: $paragraph3FontSize;
-    text-decoration: $paragraph3TextDecoration;
-    font-family: $paragraph3FontFamily;
-    font-weight: $paragraph3FontWeight;
-    font-style: $paragraph3FontStyle;
-    font-stretch: $paragraph3FontStretch;
-    letter-spacing: $paragraph3LetterSpacing;
-    line-height: $paragraph3LineHeight;
-    text-indent: $paragraph3ParagraphIndent;
-    border-spacing: $paragraph3ParagraphSpacing;
-  }
+@import "../css/base/_typography.scss";
 </style>
