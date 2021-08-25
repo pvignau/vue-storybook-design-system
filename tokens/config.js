098ce2898d9f08e0ddfe6469f2b85fc066be606b
Pierre-Yves Vignau <pyvignau@infopro-digital.com>
2021-08-25 18:32:09 +0200

    Update style dictionary config file to accept every type of token and separate them

--- tokens/config.js
+++ tokens/config.js
@@ -1,79 +1,110 @@
 const StyleDictionary = require("style-dictionary");
 
+const filters = [
+
+    {
+        name: 'colors',
+        matcher: (token) => ['color', 'gradient'].indexOf(token.attributes.category) >= 0
+    },
+    {
+        name: 'typography',
+        matcher: (token) => token.attributes.category === 'font'
+    },
+    {
+        name: 'grids',
+        matcher: token => token.attributes.category === 'grid',
+    },
+    {
+        name: 'breakpoints',
+        matcher: (token) => ['breakpoint', 'breakpoints'].indexOf(token.attributes.category) >= 0
+    },
+    {
+        name: 'sizes',
+        matcher: (token) => ['size', 'sizes'].indexOf(token.attributes.category) >= 0
+    },
+    {
+        name: 'motion',
+        matcher: token => token.attributes.category === 'motion',
+    },
+    {
+        name: 'effects',
+        matcher: token => token.attributes.category === 'effect',
+    },
+    {
+        name: 'radii',
+        matcher: (token) => ['radii', 'radius'].indexOf(token.attributes.category) >= 0
+    },
+    {
+        name: 'borders',
+        matcher: (token) => ['border', 'borders'].indexOf(token.attributes.category) >= 0
+    },
+    {
+        name: 'spacing',
+        matcher: token => token.attributes.category === 'spacing',
+    },
+]
+
+filters.forEach(filter => {
+    StyleDictionary.registerFilter({
+        name: filter.name,
+        matcher: filter.matcher,
+    });
+})
+
 StyleDictionary.registerTransform({
-    name: "size/px", // notice: the name is an override of an existing predefined method (yes, you can do it)
-    type: "value",
-    matcher: function (prop) {
-        /* supports both "pixel" and "pixels" */
+    name: 'size/px',
+    type: 'value',
+    matcher: token => {
+        return token.unit === 'pixel' && token.value !== 0
+    },
+    transformer: token => {
+        return `${token.value}px`
+    },
+})
 
-        return (
-            prop &&
-            prop.original &&
-            prop.original.unit &&
-            prop.original.unit.startsWith("pixel")
-        );
+StyleDictionary.registerTransform({
+    name: 'size/percent',
+    type: 'value',
+    matcher: token => {
+        return token.unit === 'percent' && token.value !== 0
     },
-    transformer: function (prop) {
-        return `${prop.value}px`;
+    transformer: token => {
+        return `${token.value}%`
     },
-});
+})
 
-/* Basic filter to separate typography tokens. It might need tweaking depending on the token data shape */
-StyleDictionary.registerFilter({
-    name: "isTypography",
-    matcher: function (prop) {
-        return (
-            prop.name.startsWith("headline") || prop.name.startsWith("paragraph")
-        );
-    },
-});
+StyleDictionary.registerTransformGroup({
+    name: 'custom/scss',
+    transforms: StyleDictionary.transformGroup['less'].concat([
+        'size/px',
+        'size/percent',
+    ]),
+})
 
 module.exports = {
-    source: ["./design-tokens.json"],
+    source: ["tokens/*.json"],
     platforms: {
-        js: {
+        "json-flat": {
             transformGroup: "js",
-            buildPath: "./dist/js/",
-            transforms: ["size/px", "name/cti/camel"],
-            /* We split tokens into separate files - it will be easier to use them this way */
-            files: [
-                /* Filter and extract typography tokens */
-                {
-                    destination: "typography.js",
-                    format: "javascript/es6",
-                    filter: "isTypography",
-                },
-                /* Filter and extract color tokens*/
-                {
-                    destination: "colors.js",
-                    format: "javascript/es6",
-                    filter: {
-                        type: "color",
-                    },
-                },
-            ],
+            buildPath: "dist/json/",
+            files: (() => filters.map(filter => {
+                return {
+                    "destination": `_${filter.name}.json`,
+                    "format": "json/flat",
+                    filter: filter.name
+                }
+            }))()
         },
-        sass: {
-            transformGroup: "sass",
-            buildPath: "./dist/sass/",
-            transforms: ["size/px", "name/cti/camel"],
-            /* We split tokens into separate files - it will be easier to use them this way */
-            files: [
-                /* Filter and extract typography tokens */
-                {
-                    destination: "typography.scss",
-                    format: "scss/variables",
-                    filter: "isTypography",
-                },
-                /* Filter and extract color tokens*/
-                {
-                    destination: "colors.scss",
-                    format: "scss/variables",
-                    filter: {
-                        type: "color",
-                    },
-                },
-            ],
+        "scss": {
+            transformGroup: "custom/scss",
+            buildPath: "dist/scss/",
+            files: (() => filters.map(filter => {
+                return {
+                    "destination": `_${filter.name}.scss`,
+                    "format": "scss/variables",
+                    filter: filter.name
+                }
+            }))()
         },
     },
 };
