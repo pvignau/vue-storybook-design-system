f3c1ce57b56e99a9fd1f6075f52261aee1f41a12
Pierre-Yves Vignau <vignau.py@gmail.com>
2021-08-30 16:17:22 +0200

    Add TabBar to main project main page

--- my-handsome-project/src/App.vue
+++ my-handsome-project/src/App.vue
@@ -2,16 +2,21 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="actions">
+      <TabBar></TabBar>
+    </div>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import { TabBar } from '@mhp/design-system'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    TabBar
   }
 }
 </script>
@@ -24,5 +29,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.actions {
+  width: 375px;
+  margin: 60px auto;
 }
 </style>
