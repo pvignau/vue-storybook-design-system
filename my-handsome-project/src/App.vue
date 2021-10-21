001613345c58c1ce1955c842a5d0d638f855f362
Pierre-Yves Vignau <vignau.py@gmail.com>
2021-10-21 15:49:28 +0200

    Update App to use finally correct DS

--- my-handsome-project/src/App.vue
+++ my-handsome-project/src/App.vue
@@ -1,9 +1,39 @@
 <template>
   <div id="app">
-    <my-header>
+    <my-header title="Incidents non envoyés">
       <template v-slot:action-left><my-button type="tertiary"><icon type="menu"></icon></my-button></template>
       <template v-slot:action-right><my-button type="tertiary"><icon type="help"></icon></my-button></template>
     </my-header>
+    <div class="content">
+      <card :id="352" state="ongoing" author="Guy Rophoard" date="16/04/2021">
+        <template v-slot:default><p class="text__sm">Site concerné, sur une, deux ou plusieurs lignes.</p></template>
+        <template v-slot:title>Lorem ipsum dolor sit amet, consecte adipiscing elitictu arcu ante risus elit quis egestas mollis.</template>
+        <template v-slot:subtitle>Type d'incident</template>
+        <template v-slot:actions>
+          <my-button type="secondary">Supprimer</my-button>
+          <my-button>Soumettre</my-button>
+        </template>
+      </card>
+      <card :id="352" state="draft" author="Guy Rophoard" date="16/04/2021">
+        <template v-slot:default><p class="text__sm">Site concerné, sur une, deux ou plusieurs lignes.</p></template>
+        <template v-slot:title>Lorem ipsum dolor sit amet, consecte adipiscing elitictu arcu ante risus elit quis egestas mollis.</template>
+        <template v-slot:subtitle>Type d'incident</template>
+      </card>
+      <card :id="352" state="draft" author="Guy Rophoard" date="16/04/2021">
+        <template v-slot:default><p class="text__sm">Site concerné, sur une, deux ou plusieurs lignes.</p></template>
+        <template v-slot:title>Lorem ipsum dolor sit amet, consecte adipiscing elitictu arcu ante risus elit quis egestas mollis.</template>
+        <template v-slot:subtitle>Type d'incident</template>
+        <template v-slot:actions>
+          <my-button type="secondary">Supprimer</my-button>
+          <my-button>Soumettre</my-button>
+        </template>
+      </card>
+      <card :id="352" state="draft" author="Guy Rophoard" date="16/04/2021">
+        <template v-slot:default><p class="text__sm">Site concerné, sur une, deux ou plusieurs lignes.</p></template>
+        <template v-slot:title>Lorem ipsum dolor sit amet, consecte adipiscing elitictu arcu ante risus elit quis egestas mollis.</template>
+        <template v-slot:subtitle>Type d'incident</template>
+      </card>
+    </div>
   </div>
 </template>
 
@@ -11,7 +41,8 @@
 import {
   Header as MyHeader,
   Button as MyButton,
-  Icon
+  Icon,
+  Card
 } from '@mhp/design-system'
 
 export default {
@@ -19,23 +50,23 @@ export default {
   components: {
     MyHeader,
     MyButton,
-    Icon
+    Icon,
+    Card
   }
 }
 </script>
 
 <style lang="scss">
+@import "~@mhp/design-system/src/css/app.scss";
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: $color-background;
+  color: $color-on-background;
+  overflow: auto;
 }
 
-.actions {
-  width: 375px;
-  margin: 60px auto;
+.header {
+  position: sticky;
+  top: 0;
 }
 </style>
